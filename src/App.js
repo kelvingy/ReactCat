ebfb394b6c9952286c507185289b612159b540fc
kelvingy <kelvingy@udel.edu>
2021-10-01 05:01:44 -0400

    I push my ReactApp file in the github

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+import { render } from '@testing-library/react';
+
---
+++ src/components/CatInfo.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+class CatInfo extends React.Component {
+    state={
+        BSHInfo:"British shorthair cats are very common, they are friendly, they are not particularly clingy, but they also enjoy people's care and love. They are loyal, but they don't like to stay on people's legs and prefer to squeeze next to you on the sofa. They are quieter, do not run around and jump around, and tolerate children and dogs. They don't like being hugged.",
+
+        PersianInfo:"Persian cats are sweet and gentle. They like a stable environment and are treated with gentleness. They don't like to move, play, and are quiet. They mainly rely on their big eyes to communicate. They do not require too much attention from people, can be alone at home, and are less courageous.",
+
+        RagdollInfo:"Ragdolls cat is relaxed and happy, just like a child's doll. They are friendly, quiet, and seem relatively lazy. They are not very active, nor do they like to bark. Ragdolls are known for their endurance. They can bear to put on clothes and be held and walk around without resisting. Because of their friendliness and intelligence, they are often compared with dogs.",
+
+        EgyptianInfo:"Egyptian cats are sometimes considered alienated and shy. But for the owner, they can form a special emotional bond. They are very loyal. Egyptian cat has a keen sense of smell, hearing and vision. They are shy and sensitive, and are easily disturbed by sudden noises. Egyptian cat is one of the most flexible and agile breeds. Like to be outdoors.",
+
+    }
+    render() { 
+        return <div></div>;
+    }
+}
+ 
+export default CatInfo;
